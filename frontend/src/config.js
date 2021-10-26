4b618ff5ace16d7485ee0b56dbd4de294d12759c
David Streid <david.streid.nyc@gmail.com>
2021-10-26 14:02:18 -0400

    modifying config setup

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,28 @@
-const config = {
-    root: '/swabnseq/',
-    service: '/swabnseq/'
+const properties = {
+    'base': {
+        root: '/',
+        service: '/'
+    },
+    'dev': {
+        root: '/',
+        service: 'http://localhost:5000/'
+    },
+    'qa': {
+        root: '/swabnseq/',
+        service: '/swabnseq/'
+
+    },
+    'prod': {
+        root: '/swabnseq/',
+        service: '/swabnseq/'
+
+    }
 };
+
+const env = process.env.REACT_APP_ENV.toLowerCase();
+const config = Object.assign( properties.base, properties[ env ] )
+if(env !== 'prod'){
+    console.log(`${env} ENVIRONMENT: ${JSON.stringify(config)}`);
+}
+
 export default config;
